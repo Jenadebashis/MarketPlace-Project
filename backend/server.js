f738a13273828ef31f9cd6abdadce8f9e9bc91c5
Debashis Jena <debashisjena100@gmail.com>
2026-02-08 07:21:04 +0530

    server can listen from anywhere

--- backend/server.js
+++ backend/server.js
@@ -107,6 +107,6 @@ app.post('/api/auth/login', catchAsync(async (req, res) => {
 app.use(globalErrorHandler);
 
 const PORT = 3000;
-app.listen(PORT, () => {
+app.listen(PORT, '0.0.0.0' ,() => {
   console.log(`Server is running on http://localhost:${PORT}`);
 });
