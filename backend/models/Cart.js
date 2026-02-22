d6b7c5f413a4140886144d16d1fb236fa3b773e5
Debashis Jena <debashisjena100@gmail.com>
2026-02-23 04:48:30 +0530

    updating the cartSchema

--- backend/models/Cart.js
+++ backend/models/Cart.js
@@ -5,12 +5,15 @@ const cartSchema = new mongoose.Schema({
   userId: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
   items: [
     {
-      productId: { type: String, required: true },
+      _id: { type: String, required: true }, // Changed to match your JSON key
       name: String,
       price: Number,
-      qty: { type: Number, default: 1 },
       category: String,
-      image: String
+      image: String,
+      qty: { type: Number, default: 1 },
+      vendorId: String,
+      sellerName: String,
+      specifications: mongoose.Schema.Types.Mixed
     }
   ]
 }, { timestamps: true });
